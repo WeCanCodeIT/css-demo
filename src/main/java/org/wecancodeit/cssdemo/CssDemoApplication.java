3981a8b1d6aeaea326382037846c79d5f055761c
Don Hamilton III <donhamiltoniii@gmail.com>
2018-10-04 12:18:27 -0400

    Convert static project to a Spring Boot project
    Build out a Spring Boot project from Spring Initializr
    and migrate static files into that ecosystem

---
+++ src/main/java/org/wecancodeit/cssdemo/CssDemoApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.cssdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssDemoApplication.class, args);
+	}
+}
